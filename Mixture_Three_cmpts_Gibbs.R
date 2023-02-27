cd3c7b923283f42fcc7691b22be99537eaa6997d
xc308 <xc308@exeter.ac.uk>
2023-02-27 23:59:08 +0000

    understanding

--- Mixture_Three_cmpts_Gibbs.R
+++ Mixture_Three_cmpts_Gibbs.R
@@ -80,6 +80,7 @@ NIG_update <- function(a, b, mu, Data) {
 # counter for counts in each category
 counter <- function(zs, k) {
   # counts the number of zi = j, j = 1, 2, ..., k
+  # k: length/ number of categories
   
   counts <- vector(mode = "numeric", length = k)
   for (j in 1:k) {
@@ -132,9 +133,9 @@ Gibbs_MultiCmpts <- function(ys, zs, mus, epsilon, mu0, s0, w, nsim, burnin) {
   m = length(mus) + length(w) + 3 
   
   res <- matrix(NA, nrow = n.sim, ncol = m)
-  for (i in 1:n) {
+  for (i in 1:n.sim) {
     # sample mu0 | ...
-    mu0_pst_par <- NN_update(prior.mu = mu0, prior.v = s0, like.v = epsilon, 
+    mu0_pst_par <- NN_update(prior.mu = mu0.m, prior.v = mu0.v, like.v = s0, 
                              data = mus)
     mu0 <- rnorm(1, mean = mu0_pst_par[1], sd = sqrt(mu0_pst_par[2]))
     
@@ -195,11 +196,69 @@ Gibbs_MultiCmpts <- function(ys, zs, mus, epsilon, mu0, s0, w, nsim, burnin) {
 # w,
 
 w_start = rdirichlet(1, alpha = c(1, 1, 1))
+dim(w_start) #[1] 1 3
 zs_start = replicate(length(Mix_Data), CateSampler(w_start))
+
+zs_start = replicate(9, CateSampler(w_start))
+mus_start = rnorm(3)
+mus_start[zs_start]
+
+
+mu0_start = 1  # mean para of prior on mu
+s0_start = 1  # var para of prior on mu
+mean(Mix_Data) # [1] 2.781925
+# not exactly the same as data mean, but not hugely different
+  # allow better variation and mixing and wont' take very long 
+    # to converge
+
+epsilon_start = 5 # from var(Mix_Data) # [1] 4.637941
+
+
+#------------------------
+# starting value setting
+#------------------------
+
+w_start = rdirichlet(1, c(1, 1, 1))
+zs_start = replicate(length(Mix_Data), CateSampler(probs = w_start))
+# 500 starting value for z_i, one for each yi
+#mus = (mu1, mu2, mu3)
 mus_start = rnorm(3)
 mu0_start = 1
 s0_start = 1
-epsilon_start = 5 # from var(Mix_Data) # [1] 4.637941
+epsilon_start = 5
+#var(Mix_Data) # 4.63 
+
+
+#-----------------------
+# To understand the code
+#-----------------------
+
+Mix_Data[1]
+P <- matrix(NA, nrow = 5, ncol = 3)
+for(i in 1:5) {
+  P[i, ] <- CatProbs(w = w_start, yi = Mix_Data[i], mus = mus_start,eps = 5)
+}
+P #5*3
+#          [,1]      [,2]      [,3]
+# [1,] 0.1808866 0.4063159 0.4127975
+
+apply(P, 2, CateSampler)
+# [1] 1 1 2     underestimate the zs numbers, 
+  # affect data.j <- ys[zs == j]
+  # less ys will be remained/selected for each category
+apply(P, 1, CateSampler)
+# [1] 1 3 2 3 2
+
+Mix_Data[1:5][c(T, F)] 
+# [1] 2.453943 1.965925 4.875129
+# logical assertion only 2 outcomes, data has 1:5
+# recycle, T F T F T
+
+
+
+
+
+
 
 
 Mix_Data <- read.csv("Lab8Mixture.csv", header = F)
@@ -224,9 +283,10 @@ var(Mix_Data)
 CateSampler
 cumsum(w_start)
 x <- runif(1)
-str(which(x <= cdf)[1])
 
 which(x <= cdf)[1]
+replicate(5, CateSampler(w_start))
+
 
 y <- rnorm(15)
 str(y)
