2ea1dd68cbe8dc31f67e10093e3fc5350ff0a03e
xc308 <xc308@exeter.ac.uk>
2023-03-10 22:43:18 +0000

    Metropolis for Poisson

--- Hoff_Chp10_Metropolis_AC_ratio.R
+++ Hoff_Chp10_Metropolis_AC_ratio.R
@@ -103,10 +103,274 @@ sapply(delta, function(x) Metropolis_NN(theta_0 = 0, n.sim = 1000,
 
 
 
+#==============================
+# Metropolis for Poisson model
+#==============================
+# a sample of 52 female song sparrows was studied over
+# a summer and their reproductive activities were recorded
+
+# The boxplot indicates that two-year-old birds in this population
+# had the highest median reproductive success, with the number of offspring
+# declining beyond two years of age
+
+# Aim: 
+  # to understand the relationship between age and reproductive success,
+  # or to make population forecasts for this group of birds.
+
+# Model construction:
+  # The number of off-spring of each bird is non-neg {0, 1, 2, ...}
+  # Y_i conditional on Age_i ~ Poi(theta)
+    # so model log(E[Y_i | Age_i]) = b1 + b2*Age_i + b3*Age_i^2
+
+
+#-----
+# Data set
+#-----
+head(sparrows)
+# 52 * 2
+#     fledged age
+#[1,]       3   3
+#[2,]       1   3
+#[3,]       1   1
+#[4,]       2   1
+
+## non-bayesian method 
+sparrow_dat <- as.data.frame(sparrows)
+
+fit.mle <- glm(fledged ~ age + age^2, data = sparrow_dat, family = "poisson")
+fit.mle$coefficients
+summary(fit.mle)
+
+n <- length(y)
+p <- ncol(X)
+
+SSE <- sum(fit.mle$residuals^2) # 28.8
+sigma2_ols <- 1/(n - p - 1) * SSE  # [1] 0.5997504
+
+
+# variance of Beta use ols variance of Beta:
+  # sigma2 * (t(X)X)_{inv}
+  # sigma2 use sigma2_ols plug in
+  
+
+#----------
+# prepare for Bayesian
+#----------
+
+y <- sparrows[, 1]
+
+# design matrix
+X <- cbind(rep(1, length(y)), sparrows[, -1], sparrows[, -1]^2)
+X  #52 * 3
+t(X) %*% X
+
+# data set for Bayesian
+yX <- cbind(y, X)
+colnames(yX) <- c("fledged", "intercept", "age", "age2")
+head(yX)
+
+n <- length(y)
+p <- ncol(X)
 
 
 
+#----------------------
+# Model for Metropolis
+#----------------------
+
+# the offspring for each bird i Yi 
+  # Yi|x_i ~ Poisson (exp^{b*x_i})
+
+# prior:
+  # beta = (b1, b2, b3) ~iid N(0, 100)
+
+# posterior :
+  # beta | y, X has no closed form 
+
+## resort to metropolis MC algo
 
 
+#--------------------------------------
+# Metropolis sampler for Poisson model
+#--------------------------------------
+library(mvtnorm)
+
+
+# sigma2_hat = var(log(y + 1/2)) sample variance on log scale
+  # + 1/2 to avoid log 0 = inft, as count y has 0's
+Var_beta_ols <- var(log(y + 1/2)) * solve(t(X) %*% X) 
+beta_0 <- 0  # starting beta element, will be replicate to a vector of 3
+
+prior.m.b <- rep(0, ncol(X)) # mean of prior of beta
+prior.sd.b <- rep(10, ncol(X)) # sd of prior of beta
+
+
+Metro_Poisson <- function(beta_0, n.sim, Data = y, DM = X, 
+                          burn_in = 1000, Thinning = T, thin = 10) {
+  Beta <- rep(beta_0, ncol(X))
+  #str(Beta) # num [1:3]
+  
+  BETA <- NULL
+  counts <- 0
+  
+  for (i in 1:n.sim) {
+    # proposal:
+    beta_star <- rmvnorm(1, mean = Beta, 
+                         sigma = Var_beta_ols) # [1, 1:3]
+    beta_star <- t(beta_star)
+    
+    #L <- X %*% t(beta_star) # 52*1
+    #sum(dpois(y, lambda = exp(L), log = T))
+    
+    # calculate AC ratio
+    logr <- sum(dpois(y, exp(X %*% beta_star), log = T)) - 
+      sum(dpois(y, exp(X %*% Beta), log = T)) + 
+      sum(dnorm(beta_star, prior.m.b, prior.sd.b, log = T)) - 
+      sum(dnorm(Beta, prior.m.b, prior.sd.b, log = T))
+    
+    
+    # AC or RJ
+    u <- runif(1)
+    if (log(u) < logr) {
+      Beta <- beta_star  # 3*1
+      counts <- counts + 1
+    }
+    
+    # update 
+    BETA <- rbind(BETA, t(Beta)) # 1*3
+  }
+  
+  # AC ratio
+  AC_ratio <- counts / n.sim
+  
+  if (Thinning) {
+    return(list(beta_pst = BETA[seq(burn_in, n.sim, by = thin), ], AC_ratio = AC_ratio))
+  } else{
+    return(list(beta_pst = BETA[burn_in:n.sim, ], AC_ratio = AC_ratio))
+  }
+}
+
+
+
+#-----------
+# Test on data
+#-----------
+Res <- Metro_Poisson(beta_0 = 0, n.sim = 500, Data = y, DM = X)
+
+Res$AC_ratio # 0.456
+# so the proposal is acceptable
+# now re-run with longer chain for stationary
+
+Res <- Metro_Poisson(beta_0 = 0, n.sim = 1e4, Data = y, DM = X, burn_in = 1000)
+Res$AC_ratio
+
+beta_pst <- Res$beta_pst
+str(beta_pst)
+
+#---------
+# Plot
+#--------
+par(mfrow = c(1, 3))
+for(idx in 1:3) {
+  plot(1000:1e4, beta_pst[, idx], type = "l",
+       main = bquote("Trace plot of " ~ beta[.(idx)]))
+  
+}
+
+
+par(mfrow = c(1, 3))
+for (idx in 1:3) {
+  acf(beta_pst[, idx])$acf[2]
+}
+
+
+
+lag1_acf <- acf(beta_pst[, 2])$acf[2]
+
+Res2 <- Metro_Poisson(beta_0 = 0, n.sim = 1e4, Data = y, DM = X, burn_in = 1000, 
+                      Thinning = T, thin = 10)
+Res2$AC_ratio # [1] 0.4272
+
+beta_pst2 <- Res2$beta_pst
+
+par(mfrow = c(1, 3))
+for (idx in 1:3) {
+  acf(beta_pst2[, idx])$acf[2]
+}
+
+sapply(1:3, function(x) acf(beta_pst2[, x], plot = F)$acf[2])
+# [1] 0.1817538 0.2270815  0.2549073
+
+
+library(coda)
+effectiveSize(as.mcmc(beta_pst2[, x]))
+
+sapply(1:3, function(x) effectiveSize(as.mcmc(beta_pst2[, x])))
+# from the remaining 1000 samples after 1 tenth thinning
+  # the effective sample sizes for each beta are:
+#    var1     var1     var1 
+# 609.5962 391.2855 356.7926 
+
+## after thinning, we have nearly 1000 independent samples
+
+
+## posterior marginal densities
+par(mfrow = c(1, 3))
+for(idx in 1:3) {
+  plot(density(beta_pst2[, idx]), col = "red")
+}
+
+
+plot(density(beta_pst2[, 1]), col = "red")
+
+
+
+#--------
+# Data set
+#--------
+set.seed(1)
+
+## try make sure age 2 has the largest, age 6 smallest offsp
+springs <- round(rnorm(6, 3, sqrt(2)), 0)
+Offsp <- vector("numeric", length = 6)
+
+Offsp[2] <- max(springs)
+Offsp[6] <- min(springs)
+
+idx1 <- which(springs == max(springs))[1]
+idx2 <- which(springs == min(springs))[1]
+
+Offsp[-c(2, 6)] <- springs[-c(idx1, idx2)]
+#---------------------------------------
+
+
+OFFSP <- NULL
+for (i in 1:52) {
+  springs <- round(rnorm(6, 3, sqrt(2)), 0)
+  
+  Offsp <- vector("numeric", length = 6)
+  
+  Offsp[2] <- max(springs)
+  Offsp[6] <- min(springs)
+  idx1 <- which(springs == max(springs))[1]
+  idx2 <- which(springs == min(springs))[1]
+  Offsp[-c(2, 6)] <- springs[-c(idx1, idx2)]
+  
+  OFFSP <- rbind(OFFSP, abs(Offsp))
+}
+
+OFFSP # 20 * 6
+y <- c(t(OFFSP))
+Age <- replicate(52, seq(1:6))
+Age <- c(Age)
+ID <- rep(1:52, each = 6)
+
+Offspring_dat <- data.frame(ID = ID, Age = Age, Ofsp = y)
+head(Offspring_dat, 15)
+
+
+boxplot(Offspring_dat$Ofsp ~ Offspring_dat$Age)
+# age 2 has the highest offspring number
+# age 6 has the least
 
 
